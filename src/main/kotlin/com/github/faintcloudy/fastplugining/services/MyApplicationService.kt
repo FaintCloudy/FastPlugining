a115169fdf16d7a00b61a1d093c63520e96fedae
GitHub Action <action@github.com>
2022-04-23 21:44:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/faintcloudy/fastplugining/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/faintcloudy/fastplugining/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.faintcloudy.fastplugining.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.faintcloudy.fastplugining.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/faintcloudy/fastplugining/services/MyProjectService.kt
+++ src/main/kotlin/com/github/faintcloudy/fastplugining/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.faintcloudy.fastplugining.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.faintcloudy.fastplugining.MyBundle
 
 class MyProjectService(project: Project) {
 
